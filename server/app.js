09fea5c8e5f1f24d822ce380bee90a4a50971f3a
Asit-14 <asitshakya789@gmail.com>
2025-11-02 15:52:42 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ server/app.js
@@ -0,0 +1,35 @@
+import { config } from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import route from './routes/user.routes';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
+
+app.use(morgan('dev'))
+
+app.use(cookieParser());
+
+app.use('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.use('/api/v1/user', route)
+
+app.use(/.*/, (req, res) => {
+  res.status(404).send('OOPS! 404 page not found');
+});
+
+
+
+export default app;
