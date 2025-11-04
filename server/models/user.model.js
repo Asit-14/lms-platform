0db49d521d432cec56dcbe37b7f9824da1ea307a
Asit-14 <asitshakya789@gmail.com>
2025-11-04 20:56:36 +0530

    Refactor user model schema and add AppError utility class for improved error handling

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -7,25 +7,44 @@ const userSchema = new Schema({
         required: [true, 'Name is required'],
         minLength: [5, "Name must be 5 character"],
         maxLength: [50, "Name shoud be  50 character"],
-        trim :true
-        
-        
-              },
-             email: {
-            type: String,
-            required: [true, 'Email is required'],
-            lowercase: true,
-            trim: true,
-            unique: true,
-            match: [
-              /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
-              'Please enter a valid email address'
-            ]
-},
+        trim: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        lowercase: true,
+        trim: true,
+        unique: true,
+        match: [
+            /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
+            'Please enter a valid email address'
+        ]
+    },
     password: {
-        type:'String'
-    }
-})
+        type: 'String',
+        require: [true, 'Password  is  required'],
+        minLength: [8, 'Password  should  be 8 character'],
+        select: false,
+    },
+    avatar: {
+        public_id: {
+            type: 'String'
+        },
+        secure_url: {
+            type: 'String'
+        }
+    },
+    role: {
+        type: 'String',
+        enum: ['USER', 'ADMIN'],
+        default:'USER'
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date
+}, {
+    timestamps:true
+});
 
 const User = model('User', userSchema);
 export default User;
